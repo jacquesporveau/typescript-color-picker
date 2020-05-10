7edbe827df46ebb0a0e35464449f9338119b8a79
Dennis Marchand <dennis@juul.com>
2020-05-10 12:11:58 -0700

    Use Websafe Colors

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,20 @@
 import React, { FC } from 'react';
 
-const roygbiv = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const websafeColors = (): string[] => {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
+  const radix = 16;
+  const colors = []
+
+  for (let red = 0; red < 16; red += 3) {
+    for (let green = 0; green < 16; green += 3) {
+      for (let blue = 0; blue < 16; blue += 3) {
+        colors.push(`#${red.toString(radix)}${green.toString(radix)}${blue.toString(radix)}`);
+      }
+    }
+  }
+
+  return colors
+}
 
 interface Props {
   onClick: (color: string) => void;
@@ -9,7 +23,7 @@ interface Props {
 export const ColorPicker: FC<Props> = ({ onClick }) => {
   return (
     <>
-      {roygbiv.map(color => {
+      {websafeColors().map(color => {
         return (
           <div
             key={color}
